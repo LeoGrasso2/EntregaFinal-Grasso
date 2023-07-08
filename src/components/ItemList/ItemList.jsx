247f6930c5866a720dca5014e6c015caae950e07
LeoGrasso2 <grassomleo@gmail.com>
2023-07-07 21:56:02 -0300

    agregado error/warning en caso de url invalida

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -10,28 +10,39 @@ import { useParams } from 'react-router-dom';
 const ItemList = () => {
     let { id: cat } = useParams();
     const [pokeInfo, setPokeInfo] = useState()
+    const [error, setError] = useState(null) 
     let list = []
 
     useEffect(() => {
         const getItems = async () => {
-            const q = query(collection(db, cat), orderBy('ide', 'asc'))
-            const querySnapshot = await getDocs(q);
-            const items = []
-            querySnapshot.forEach((doc) => {
-                items.push({...doc.data(), id: doc.id})
-            })
+            try{
+                const q = query(collection(db, cat), orderBy('ide', 'asc'))
+                const querySnapshot = await getDocs(q);
+                const items = []
+                querySnapshot.forEach((doc) => {
+                    items.push({...doc.data(), id: doc.id})
+                })
 
-            setPokeInfo(items)
+                setPokeInfo(items)
+            } catch (err) {
+                setError(err)
+            }
         }
-        getItems();
         
+        getItems();
     },[cat])
 
     const renderList = () => {
         return pokeInfo?.map(data => (
             <Item item={data} key={data.id}></Item>
         )) 
-}
+    }
+
+    if(error){
+        return <h5 className='align-self-center'>Navega por una de las categorias o productos disponibles.</h5>
+    }
+    
+
     return (
         <>
         <div className="d-flex">
